dbd1d1c6da1ee3542f77b6aa9239c6f6a0d8512e
MakenzieBuchenroth-Neumont <mbuchenroth@student.neumont.edu>
2025-01-21 10:02:05 -0700

    Finished ArtManager card ASCII art

--- Blackjack/ArtManager.cs
+++ Blackjack/ArtManager.cs
@@ -242,134 +242,243 @@ public class ArtManager {
 			 |____ꓘ|");
 		}
 	#endregion
-	#region Clubs
-	public void AceOfClubs() {
-		console.WriteLine(@"
-      _____
-      |A _  | _____
-    |  |  || \ / || ( ) ||A_ _ |
-         |____V||  .  ||(_'_)||( v )|
-                |____V||  |  || \ / |
-                       |____V|");
+		#region Clubs
+		public void AceOfClubs() {
+			console.WriteLine(@"
+		  _____
+		  |A _  | 
+		  | ( ) |
+		  |(_'_)|
+		  |  |  |
+		  |____V|");
+		}
+		public void TwoOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |2 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____ᘔ|");
+		}
+		public void ThreeOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |3 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____Ɛ|");
+		}
+		public void FourOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |4 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____߈|");
+		}
+		public void FiveOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |5 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____5|");
+		}
+		public void SixOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |6 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____9|");
+		}
+		public void SevenOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |7 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |___ㄥ|");
+		}
+		public void EightOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |8 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____8|");
+		}
+		public void NineOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |9 _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____6|");
+		}
+		public void TenOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |10_  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |___0⇂|");
+		}
+		public void JackOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |J _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____ᒋ|");
+		}
+		public void QueenOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |Q _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____ტ|");
+		}
+		public void KingOfClubs() {
+			console.WriteLine(@"
+		  _____
+		 |K _  | 
+		 | ( ) |
+		 |(_'_)|
+		 |  |  |
+		 |____ꓘ|");
+		}
+		#endregion
+		#region Hearts
+		public void AceOfHearts() {
+			console.WriteLine(@"
+		   _____
+		  |A_ _ | 
+		  |( v )|
+		  | \ / |
+		  |  .  |	
+		  |____V|");
+		}
+		public void TwoOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |2_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____ᘔ|");
+		}
+		public void ThreeOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |3_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____Ɛ|");
+		}
+		public void FourOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |4_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____߈|");
+		}
+		public void FiveOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |5_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____5|");
+		}
+		public void SixOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |6_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____9|");
+		}
+		public void SevenOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |7_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |___ㄥ|");
+		}
+		public void EightOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |8_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____8|");
+		}
+		public void NineOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |9_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____6|");
+		}
+		public void TenOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |10_ _| 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |___0⇂|");
+		}
+		public void JackOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |J_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____ᒋ|");
+		}
+		public void QueenOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |Q_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____ტ|");
+		}
+		public void KingOfHearts() {
+			console.WriteLine(@"
+		  _____
+		 |K_ _ | 
+		 |( v )|
+		 | \ / |
+		 |  .  |
+		 |____ꓘ|");
+		}
+		#endregion
 	}
-	public void TwoOfClubs() {
-		console.WriteLine(@"
-      _____
-     |2 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____ᘔ|");
-	}
-	public void ThreeOfClubs() {
-		console.WriteLine(@"
-      _____
-     |3 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____Ɛ|");
-	}
-	public void FourOfClubs() {
-		console.WriteLine(@"
-      _____
-     |4 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____߈|");
-	}
-	public void FiveOfClubs() {
-		console.WriteLine(@"
-      _____
-     |5 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____5|");
-	}
-	public void SixOfClubs() {
-		console.WriteLine(@"
-      _____
-     |6 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____9|");
-	}
-	public void SevenOfClubs() {
-		console.WriteLine(@"
-      _____
-     |7 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |___ㄥ|");
-	}
-	public void EightOfClubs() {
-		console.WriteLine(@"
-      _____
-     |8 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____8|");
-	}
-	public void NineOfClubs() {
-		console.WriteLine(@"
-      _____
-     |9 ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____6|");
-	}
-	public void TenOfClubs() {
-		console.WriteLine(@"
-      _____
-     |10 ^ | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |___0⇂|");
-	}
-	public void JackOfClubs() {
-		console.WriteLine(@"
-      _____
-     |J ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____ᒋ|");
-	}
-	public void QueenOfClubs() {
-		console.WriteLine(@"
-      _____
-     |Q ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____ტ|");
-	}
-	public void KingOfClubs() {
-		console.WriteLine(@"
-      _____
-     |K ^  | 
-     | / \ |
-     | \ / |
-     |  .  |
-     |____ꓘ|");
-	}
-	#endregion
-	#region Hearts
-	public void AceOfHearts() {
-		console.WriteLine(@"
-  _____
- |A ^  | 
- | / \ |
- | \ / |
- |  .  |
- |____Ɐ|");
-	}
-}
 }
