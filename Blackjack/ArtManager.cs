e59cdeaf3639bcce69f130000a0bae10d8d3f7a7
lstevensN <lstevens@student.neumont.edu>
2025-01-21 11:25:13 -0700

    Added BetManager.cs & CardManager.cs

--- Blackjack/ArtManager.cs
+++ Blackjack/ArtManager.cs
@@ -1,483 +1,535 @@
 ﻿using System;
 
 public class ArtManager {
-	public ArtManager() {
-		#region Spades
-		void AceOfSpades() {
-			Console.WriteLine(@"
+
+	public ArtManager() {}
+
+    #region Spades
+    public static void AceOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|A .  |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|____Ɐ|");
-		}
-
-		void TwoOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void TwoOfSpades()
+    {
+        Console.WriteLine(@"
 					 _____
 					|2 .  |
 					| /.\ |
 					|(_._)|
 					|  |  |
 					|____ᘔ|");
-		}
-		void ThreeOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void ThreeOfSpades()
+    {
+        Console.WriteLine(@"
 					 _____
 					|3 .  |
 					| /.\ |
 					|(_._)|
 					|  |  |
 					|____Ɛ|");
-		}
-		void FourOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void FourOfSpades()
+    {
+        Console.WriteLine(@"
 					 _____
 					|4 .  |
 					| /.\ |
 					|(_._)|
 					|  |  |
 					|____߈|");
-		}
-		void FiveOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void FiveOfSpades()
+    {
+        Console.WriteLine(@"
 					 _____
 					|5 .  |
 					| /.\ |
 					|(_._)|
 					|  |  |
 					|____5|");
-		}
-		void SixOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void SixOfSpades()
+    {
+        Console.WriteLine(@"
 					 _____
 					|6 .  |
 					| /.\ |
 					|(_._)|
 					|  |  |
 					|____9|");
-		}
-		void SevenOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void SevenOfSpades()
+    {
+        Console.WriteLine(@"
 					 _____
 					|7 .  |
 					| /.\ |
 					|(_._)|
 					|  |  |
 					|____L|");
-		}
-		void EightOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void EightOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|8 .  |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|____8|");
-		}
-		void NineOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void NineOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|9 .  |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|____6|");
-		}
-		void TenOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void TenOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|10 . |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|___0⇂|");
-		}
-		void JackOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void JackOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|J .  |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|____ᒋ|");
-		}
-		void QueenOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void QueenOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|Q .  |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|____ტ|");
-		}
-		void KingOfSpades() {
-			Console.WriteLine(@"
+    }
+    public static void KingOfSpades()
+    {
+        Console.WriteLine(@"
 				 _____
 				|K .  |
 				| /.\ |
 				|(_._)|
 				|  |  |
 				|____ꓘ|");
-		}
-	#endregion
-		#region Diamonds
-		void AceOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    #endregion
+    #region Diamonds
+    public static void AceOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |A ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____Ɐ|");
-		}
-		void TwoOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void TwoOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |2 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____ᘔ|");
-		}
-		void ThreeOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void ThreeOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |3 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____Ɛ|");
-		}
-		void FourOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void FourOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |4 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____߈|");
-		}
-		void FiveOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void FiveOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |5 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____5|");
-		}
-		void SixOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void SixOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |6 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____9|");
-		}
-		void SevenOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void SevenOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |7 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |___ㄥ|");
-		}
-		void EightOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void EightOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |8 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____8|");
-		}
-		void NineOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void NineOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |9 ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____6|");
-		}
-		void TenOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void TenOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |10 ^ | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |___0⇂|");
-		}
-		void JackOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void JackOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |J ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____ᒋ|");
-		}
-		void QueenOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void QueenOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |Q ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____ტ|");
-		}
-		void KingOfDiamonds() {
-			Console.WriteLine(@"
+    }
+    public static void KingOfDiamonds()
+    {
+        Console.WriteLine(@"
 			  _____
 			 |K ^  | 
 			 | / \ |
 			 | \ / |
 			 |  .  |
 			 |____ꓘ|");
-		}
-	#endregion
-		#region Clubs
-		void AceOfClubs() {
-			Console.WriteLine(@"
+    }
+    #endregion
+    #region Clubs
+    public static void AceOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		  |A _  | 
 		  | ( ) |
 		  |(_'_)|
 		  |  |  |
 		  |____V|");
-		}
-		void TwoOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void TwoOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |2 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____ᘔ|");
-		}
-		void ThreeOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void ThreeOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |3 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____Ɛ|");
-		}
-		void FourOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void FourOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |4 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____߈|");
-		}
-		void FiveOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void FiveOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |5 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____5|");
-		}
-		void SixOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void SixOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |6 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____9|");
-		}
-		void SevenOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void SevenOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |7 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |___ㄥ|");
-		}
-		void EightOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void EightOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |8 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____8|");
-		}
-		void NineOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void NineOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |9 _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____6|");
-		}
-		void TenOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void TenOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |10_  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |___0⇂|");
-		}
-		void JackOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void JackOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |J _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____ᒋ|");
-		}
-		void QueenOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void QueenOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |Q _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____ტ|");
-		}
-		void KingOfClubs() {
-			Console.WriteLine(@"
+    }
+    public static void KingOfClubs()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |K _  | 
 		 | ( ) |
 		 |(_'_)|
 		 |  |  |
 		 |____ꓘ|");
-		}
-		#endregion
-		#region Hearts
-		void AceOfHearts() {
-			Console.WriteLine(@"
+    }
+    #endregion
+    #region Hearts
+    public static void AceOfHearts()
+    {
+        Console.WriteLine(@"
 		   _____
 		  |A_ _ | 
 		  |( v )|
 		  | \ / |
 		  |  .  |	
 		  |____V|");
-		}
-		void TwoOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void TwoOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |2_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____ᘔ|");
-		}
-		void ThreeOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void ThreeOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |3_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____Ɛ|");
-		}
-		void FourOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void FourOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |4_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____߈|");
-		}
-		void FiveOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void FiveOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |5_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____5|");
-		}
-		void SixOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void SixOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |6_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____9|");
-		}
-		void SevenOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void SevenOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |7_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |___ㄥ|");
-		}
-		void EightOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void EightOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |8_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____8|");
-		}
-		void NineOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void NineOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |9_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____6|");
-		}
-		void TenOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void TenOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |10_ _| 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |___0⇂|");
-		}
-		void JackOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void JackOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |J_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____ᒋ|");
-		}
-		void QueenOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void QueenOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |Q_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____ტ|");
-		}
-		void KingOfHearts() {
-			Console.WriteLine(@"
+    }
+    public static void KingOfHearts()
+    {
+        Console.WriteLine(@"
 		  _____
 		 |K_ _ | 
 		 |( v )|
 		 | \ / |
 		 |  .  |
 		 |____ꓘ|");
-		}
-		#endregion
-	}
+    }
+	#endregion
 }
