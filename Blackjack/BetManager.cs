3a184cf9d0f032468199410c5c0cbcedd4df22ec
KFrodente <kianfrodente@gmail.com>
2025-01-22 09:28:35 -0700

    Betting functions

--- Blackjack/BetManager.cs
+++ Blackjack/BetManager.cs
@@ -2,5 +2,47 @@
 
 public class BetManager
 {
+	public int playerChipCount { get; private set;}
+
+	public int currentBet { get; private set;}
+
 	public BetManager() {}
+
+
+
+	/// <summary>
+	/// Send in the amount of chips the player bet.
+	/// If the amount is greater than the player's current chips it will automatically lower it to the proper amount.
+	/// 
+	///  RETURNS: The actual amount the player was able to bet
+	/// </summary>
+	public int PlaceBet(int amount)
+	{
+		if (amount > playerChipCount)
+		{
+			currentBet = playerChipCount;
+		}
+		else
+		{
+			currentBet = amount;
+		}
+
+		return currentBet;
+	}
+
+
+
+	/// <summary>
+	/// Call when the round finishes, properly sets player's chips based on if the player won or not.
+	/// 
+	///  RETURNS: Player's original bet.
+	/// </summary>
+	public int RoundFinished(bool playerWon)
+	{
+		playerChipCount += (playerWon) ? currentBet : -currentBet;
+
+		return currentBet;
+	}
+
+
 }
