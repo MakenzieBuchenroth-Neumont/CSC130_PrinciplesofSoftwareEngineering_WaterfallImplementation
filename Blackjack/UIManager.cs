75f626fc5ac86e29fb30199c6f7d3271aed23ca6
MakenzieBuchenroth-Neumont <mbuchenroth@student.neumont.edu>
2025-01-22 09:16:17 -0700

    Got comments in UIManager to show how it will probably work

---
+++ Blackjack/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class UIManager {
+	public UIManager() {
+		CardManger card = new CardManger();
+		ArtManager art = new ArtManager();
+
+		// e.g
+		// card.aceOfspades.art = art.AceOfSpades()
+		// card.twoOfSpades.art = art.TwoOfSpades()
+	}
+}
