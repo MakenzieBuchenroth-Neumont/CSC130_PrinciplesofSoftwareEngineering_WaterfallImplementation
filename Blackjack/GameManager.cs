b914b458f1465c72f6d0907ff09290b90bd70f06
lbailey52 <160799070+lbailey52@users.noreply.github.com>
2025-01-23 10:11:53 -0700

    game manager update

--- Blackjack/GameManager.cs
+++ Blackjack/GameManager.cs
@@ -1,27 +1,24 @@
 using System;
 public class GameManager
 {
-    bool keepPlaying;
-    int playersAmount;
- 
+    bool keepPlaying = true;
+
+    // Bet Manager
+    BetManager betManager = new BetManager();
+
+    // Card Manager
+    CardManager cardManager = new CardManager();
+
     public GameManager()
     {
-        // Bet Manager
-        BetManager betManager = new BetManager();
- 
-        // Card Manager
-        CardManager cardManager = new CardManager();
- 
-        keepPlaying = true;
-        playersAmount = 1000;
     }
- 
+
     public void PlayGame()
     {
- 
+
         while (keepPlaying)
         {
-            if (playersAmount <= 0)
+            if (betManager.playerChipCount <= 0)
             {
                 keepPlaying = false;
             }
@@ -40,36 +37,87 @@ public class GameManager
                     break;
             }
         }
- 
+
     }
- 
+
     public void PlayRound()
     {
+        bool playerWon = false;
         // bet manager
-        //  place bet
- 
+        Console.WriteLine("How much would you like to bet?");
+        string betInput = Console.ReadLine();
+        int parsedBet = int.Parse(betInput);
+        parsedBet = betManager.PlaceBet(parsedBet);
+
+
         // card manager
-        // shuffle deck
-        // deal to player and dealer
- 
+        cardManager.StartGame();
+
         // display
         DisplayHands();
- 
+        while (cardManager.playerStand != true)
+        {
+            HitOrStand();
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    cardManager.PlayerTurn(true);
+                    break;
+                case "2":
+                    cardManager.PlayerTurn(false);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, try again");
+                    break;
+            }
+
+        }
+
+        while (cardManager.dealerStand != true)
+        {
+            cardManager.DealerTurn(true);
+        }
+        cardManager.Results();
+
+        betManager.RoundFinished(cardManager.playerWon);
+
     }
- 
+
     public void DisplayHands()
     {
         // ascii display
+        foreach (Card card in cardManager.dealerDeck)
+        {
+            card.art();
+        }
+
+
+        foreach (Card card in cardManager.playerDeck)
+        {
+            card.art();
+        }
+
+
+
     }
- 
- 
- 
+
+    public void HitOrStand()
+    {
+        Console.WriteLine("Hit or Stand?");
+        Console.WriteLine("1 - Hit");
+        Console.WriteLine("2 - Stand");
+
+    }
+
+
+
     public void DisplayGameMenu()
     {
         Console.WriteLine("Welcome to Black Jack! Would you like to place a bet?");
         Console.WriteLine("1 - Yes, I'd like to place a bet");
         Console.WriteLine("2 - No thanks.");
- 
+
     }
- 
+
 }
